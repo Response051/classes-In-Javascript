ffc06bd7fdb8a3992b8db2c9317cff8c9d97164c
IbeawuchiSopuru <>
2023-01-06 08:37:00 +0000

    This commit is to check the simple use case for Tenary Operators

---
+++ Tenary Operators/README.md
@@ -0,0 +1,2 @@
+THis program seeks to check the proficency of the student on Tenary operators.
+it checks if the inputed number is positive or negative
---
+++ Tenary Operators/Tindex.js
@@ -0,0 +1,11 @@
+/***
+1. the console should print if the inputed value is a positive or a negative number.
+2. use the Tenary operators
+*/
+
+let value = prompt("enter Number, ");
+let output = (value >= 0) ? "positive" : "negative";
+console.log(output);
+
+
+module.exports = { console }
