73dea207f078248452dad0ebc07c6c5d41270fb1
IbeawuchiSopuru <>
2023-01-08 18:15:41 +0000

    A simple object to check the for loop

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // const simple = require ("./Classes/Learnable/index")
 // const obj1 = require("./Objects/obj1")
- const obj2 = require("./Objects/obj2")
+// const obj2 = require("./Objects/obj2")
+const obj3 = require("./Objects/Obj3/obj3")
