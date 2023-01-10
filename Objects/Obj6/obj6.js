7b5f5961bb05ffb5348c858809a1ccf0346c7ef3
IbeawuchiSopuru <>
2023-01-10 09:31:30 +0000

    A function that checks if 2 objects are same

---
+++ Objects/Obj6/obj6.js
@@ -0,0 +1,22 @@
+/***
+Is it possible to create functions A and B so that new A() == new B()?
+if yes, render the function
+*/
+let object = {};
+function A(name) {
+  this.name = name;
+  return object;
+}
+function B(name) {
+  this.name = name;
+  return object;
+}
+
+let a = new A()
+let b = new B()
+
+if (a == b) {
+  alert(true)
+}
+
+module.exports = { console }
